a4c4b20e32f94c7c6ec31a57c7ac3d9e90d7706d
Patrick Gommers <xxalfa@gmail.com>
2025-01-10 12:38:14 +0100

    Update screen.minecraft.sh

--- screen.minecraft.sh
+++ screen.minecraft.sh
@@ -1,55 +1,25 @@
 #!/bin/sh
-
-# git clone --depth 1 https://github.com/AlphaOne89/raspberry.pi.minecraft minecraft;
-
-# cd minecraft && sh screen.minecraft.sh;
-
 minecraft_file_path='https://papermc.io/api/v1/paper/1.17.1/latest/download';
-
-if [ ! -f "paperclip.jar" ]; then
-
-    apt-get install openjdk-16-jre-headless screen --yes;
-
-    wget --output-document=paperclip.jar ${minecraft_file_path}
-
-    echo "eula=true" > eula.txt;
-
+if [ ! -f "server.jar" ]; then
+apt-get install default-jre screen --yes;
+wget --output-document=server.jar ${minecraft_file_path}
+echo "eula=true" > eula.txt;
 fi
-
-if [ -f "paperclip.jar" ] && [ "$1" = 'update' ]; then
-
-    mv paperclip.jar download;
-
-    wget --timestamping --no-if-modified-since ${minecraft_file_path}
-
-    mv download paperclip.jar;
-
+if [ -f "server.jar" ] && [ "$1" = 'update' ]; then
+mv server.jar download;
+wget --timestamping --no-if-modified-since ${minecraft_file_path}
+mv download server.jar;
 fi
-
 if [ ! -f "server.properties" ]; then
-
-    read -p 'Type in a server name: ' input_prompt;
-
-    echo "server-name=${input_prompt}" >> server.properties;
-
-    echo "motd=${input_prompt}" >> server.properties;
-
+read -p 'Type in a server name: ' input_prompt;
+echo "server-name=${input_prompt}" >> server.properties;
+echo "motd=${input_prompt}" >> server.properties;
 fi
-
 screen_counter=$(screen -list | grep --count --only-matching --extended-regexp --regexp="screen.window.minecraft.one");
-
 if [ ${screen_counter} -eq 0 ]; then
-
-    memory=2500;
-
-    if [ $(awk '/MemTotal/ { printf "%.0f\n", $2/1024 }' /proc/meminfo) -lt 3000 ]; then
-
-      memory=800;
-
-    fi
-
-    /usr/bin/screen -dmSU screen.window.minecraft.one /usr/bin/java -jar -Xms${memory}M -Xmx${memory}M -Dcom.mojang.eula.agree=true paperclip.jar;
-
+memory=2500;
+/usr/bin/screen -dmSU screen.window.minecraft.one /usr/bin/java -jar -Xms${memory}M -Xmx${memory}M -Dcom.mojang.eula.agree=true server.jar;
 fi
-
 screen -r screen.window.minecraft.one
+# screen -S screen.window.minecraft.one -p 0 -X stuff "^C";
+# screen -S screen.window.minecraft.one -p 0 -X kill;
